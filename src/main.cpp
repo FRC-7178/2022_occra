6360f1bd70a716cefd457d5d814c4d2616f1b8fc
FRC-7178 <7178robotics@gmail.com>
2022-09-29 19:58:22 -0400

    Drive train done(not tested)

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,15 @@
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// Controller2          controller                    
+// Drivetrain           drivetrain    1, 2, 3, 4      
+// frontRight           motor         5               
+// frontLeft            motor         6               
+// backRight            motor         7               
+// backLeft             motor         8               
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -17,5 +26,15 @@ using namespace vex;
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-  
+  while(true) 
+  {
+    int forward = Controller1.Axis3.position(vex::percent);
+    int sideways = Controller1.Axis4.position(vex::percent);
+    int turn = Controller1.Axis1.position(vex::percent);
+
+    frontRight.spin(vex::forward, forward - sideways + turn, vex::percent);
+    frontLeft.spin(vex::forward,  forward + sideways - turn, vex::percent);
+    backRight.spin(vex::forward,  forward + sideways + turn, vex::percent);
+    backLeft.spin(vex::forward,   forward - sideways - turn, vex::percent);
+  }
 }
