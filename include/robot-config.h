81302c69a39554bcd96152efa286c45d0142cfb8
FRC-7178 <7178robotics@gmail.com>
2022-09-29 19:55:08 -0400

    Drive train done(not tested)

--- include/robot-config.h
+++ include/robot-config.h
@@ -5,6 +5,11 @@ extern brain Brain;
 // VEXcode devices
 extern controller Controller1;
 extern controller Controller2;
+extern drivetrain Drivetrain;
+extern motor frontRight;
+extern motor frontLeft;
+extern motor backRight;
+extern motor backLeft;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
