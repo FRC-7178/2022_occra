dab2ee3c0ff5e1539b3a1945e2c7f85ee7749123
FRC-7178 <7178robotics@gmail.com>
2022-10-24 06:57:19 -0400

    updated to comp robot-config.cpp

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -10,18 +10,16 @@ brain  Brain;
 // VEXcode device constructors
 controller Controller1 = controller(primary);
 controller Controller2 = controller(partner);
-// motor29 leftMotorA = motor29(Brain.ThreeWirePort.A,  false);
-// motor leftMotorB = motor(PORT2, ratio18_1, false);
-// motor_group LeftDriveSmart = motor_group(leftMotorA, leftMotorB);
-// motor rightMotorA = motor(PORT3, ratio18_1, true);
-// motor rightMotorB = motor(PORT4, ratio18_1, true);
-// motor_group RightDriveSmart = motor_group(rightMotorA, rightMotorB);
-// drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 478.78, 21.59, 406.4, mm, 8);
-pwm_out frontRight = pwm_out(Brain.ThreeWirePort.A);
-pwm_out frontLeft = pwm_out(Brain.ThreeWirePort.B);
-pwm_out backRight = pwm_out(Brain.ThreeWirePort.C);
-pwm_out backLeft = pwm_out(Brain.ThreeWirePort.D);
-//motor29 Motor393A = motor29(Brain.ThreeWirePort.A, false);
+
+pwm_out frontRight = pwm_out(Brain.ThreeWirePort.E);
+pwm_out frontLeft = pwm_out(Brain.ThreeWirePort.C);
+pwm_out backRight = pwm_out(Brain.ThreeWirePort.G);
+pwm_out backLeft = pwm_out(Brain.ThreeWirePort.B);
+pwm_out intake = pwm_out(Brain.ThreeWirePort.F);
+pwm_out armMotor = pwm_out(Brain.ThreeWirePort.D);
+// encoder armEncoder = encoder(Brain.ThreeWirePort.H);
+pot armPot = pot(Brain.ThreeWirePort.H);
+
 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
